4ae00d33a689795e48de466ee039df7f59a1ffdc
Charles <gilliamcc@gmail.com>
2017-01-17 18:23:20 -0500

    Remove unneeded comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # list of documents
-  #   metadata fields only
-  # private upload endpoint
-
-  root to: 'documents#index'
-
   resources :documents, only: [:index, :show]
 
   namespace :public do
