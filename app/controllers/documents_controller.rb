bb51996975d6f5e9ca3ec305ddcbbf01b2ebea8c
Charles <gilliamcc@gmail.com>
2017-02-26 18:28:27 -0500

    First cut of search

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -15,6 +15,38 @@ class DocumentsController < ApplicationController
     create_doc(params)
   end
 
+  def search
+    docs = Document
+
+    if !params[:item_type].nil?
+      doc_ids = MetadataGroup.where(name: params[:item_type]).pluck(:document)
+      docs = docs.where(:id.in => doc_ids)
+    end
+
+    if !params[:fields].nil?
+      names = params[:fields].keys
+      values = params[:fields]# hash of names to values
+      group_ids = []
+      fields = MetadataField.where(:name.in => names).each do |field|
+        group_ids << field.metadata_group_id if values[field.name] == field.value # add function for this in MetadataField to take care of the date
+      end
+      doc_ids = MetadataGroup.where(:id.in => group_ids).pluck(:document)
+      docs = docs.where(:id.in => doc_ids)
+    end
+
+    if !params[:tags].nil?
+      doc_ids = Tag.where(:name.in => params[:tags]).pluck(:document)
+      docs = docs.where(:id.in => doc_ids)
+    end
+
+    if !params[:description].nil?
+      docs = docs.where(description: /#{params[:description]}/)
+    end
+
+    docs_to_show = docs.paginate(page: params[:page], per_page: 10)
+    render json: docs_to_show, meta: pagination_dict(docs_to_show), root: 'documents'
+  end
+
   def show_description
     render json: { document: { description: document.description } }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :documents, only: [:index, :show, :create] do
+    collection do
+      get :search
+    end
+    
     member do
       get :description, to: 'documents#show_description'
       put :description, to: 'documents#update_description'
