a38cefbdc87ada6e71b24b5b5cf5266269ba5ed8
Charles <gilliamcc@gmail.com>
2017-03-12 23:54:51 -0400

    blah

--- app/controllers/public/documents_controller.rb
+++ app/controllers/public/documents_controller.rb
@@ -1,8 +1,7 @@
 class Public::DocumentsController < ApplicationController
   include PublicAccessibleController
-  
+
   skip_before_action :authenticate_request
-  before_action :authenticate_api_key
 
   def create
     create_doc(params)
--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -1,8 +1,7 @@
 class Public::GroupsController < ApplicationController
   include PublicAccessibleController
-  
+
   skip_before_action :authenticate_request
-  before_action :authenticate_api_key
 
   def index
     show_all_groups
