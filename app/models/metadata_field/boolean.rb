c33d95b84e8003c44420ecae2e93d97542837618
Robert Edwards <me@robertedwards.rocks>
2017-05-07 12:54:23 -0400

    Style fixes

--- app/models/metadata_field/boolean.rb
+++ app/models/metadata_field/boolean.rb
@@ -3,7 +3,7 @@ class MetadataField::Boolean < MetadataField
   # TODO: Actually use below options array, it should be used to generate the select options in the frontend
   # Once in place it can be used for ENUM types, just will be dynamically created options rather than static
   OPTIONS = [
-              { :label => 'false', :value => 0 },
-              { :label => 'true', :value => 1 },
-            ].freeze
+              { label: 'false', value: 0 },
+              { label: 'true', value: 1 }
+  ].freeze
 end
