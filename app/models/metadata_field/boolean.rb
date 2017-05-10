ea9436cd116a38a256ba0b85763036252db07548
Robert Edwards <me@robertedwards.rocks>
2017-05-09 22:19:37 -0400

    Remove options array

--- app/models/metadata_field/boolean.rb
+++ app/models/metadata_field/boolean.rb
@@ -1,9 +1,3 @@
 class MetadataField::Boolean < MetadataField
   TYPE = 'true/false'.freeze
-  # TODO: Actually use below options array, it should be used to generate the select options in the frontend
-  # Once in place it can be used for ENUM types, just will be dynamically created options rather than static
-  OPTIONS = [
-    { label: 'false', value: 0 },
-    { label: 'true', value: 1 }
-  ].freeze
 end
